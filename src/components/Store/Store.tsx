f5c7cb9307ae18b8abf21ce05179d27857fc2ec8
Pranjal <pranjalgupta2570@gmail.com>
2024-03-05 23:03:56 +0530

    search functionality

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -1,66 +1,76 @@
-import { useEffect, useState } from 'react'
-import "./Store.css"
-import { IoSearch } from "react-icons/io5";
+import React, { useEffect, useState } from 'react';
+import { IoSearch } from 'react-icons/io5';
+import './Store.css';
+
 const url = 'https://superhero-search.p.rapidapi.com/api/heroes';
 const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': 'd95ebbc835mshf0e63ed83513e45p18dc89jsnd4b76f017221',
-    'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com',
+  },
 };
 
+interface Hero {
+  id: number;
+  name: string;
+  slug: string;
+  images: { lg: string };
+}
 
 const Store = () => {
-  const [data, setData] = useState([]);
-  const [search, setSearch] = useState("");
-
-  const fetchbooks = async (api: never) => {
-    try {
-      const response = await fetch(api, options);
-      const result = await response.json();
-      console.log(result);
-      setData(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const [data, setData] = useState<Hero[]>([]);
+  const [search, setSearch] = useState<string>('');
 
   useEffect(() => {
-    fetchbooks(url);
-  }, [])
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
 
-  const searchItem = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e);
-  }
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
 
-  interface apidata {
-    id: number;
-    name: string;
-    slug: never;
-    images: undefined
-  }
+  const filteredData = data.filter((hero) =>
+    hero.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="store-content">
       <form className="search">
-        <input type="text" className="text" placeholder="Search here" value={search} onChange={(e) => searchItem(e.target.value)} />
-        <button className="button-search"><IoSearch /></button>
+        <input
+          type="text"
+          className="text"
+          placeholder="Search here"
+          value={search}
+          onChange={handleSearch}
+        />
+        <button className="button-search">
+          <IoSearch />
+        </button>
       </form>
       <div className="card">
-        {
-          data.map((item: apidata) => (
-            <div className="card-compo"
-              key={item.id}>
-              <div className="img-cont" >
-                <img src={item.images.lg} />
-                <span className="card-span" style={{ padding: "15px" }}>{item.name}</span>
-              </div>
+        {filteredData.map((hero) => (
+          <div className="card-compo" key={hero.id}>
+            <div className="img-cont">
+              <img src={hero.images.lg} alt={hero.name} />
+              <span className="card-span" style={{ padding: '15px' }}>
+                {hero.name}
+              </span>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Store
+export default Store;
